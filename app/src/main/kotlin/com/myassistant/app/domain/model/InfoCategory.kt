ab5f6226de8238fb78967e0341fb6c6b0cb96233
duhu <duhu@live.com>
2026-05-27 10:22:27 +0800

    Add app/src/main/kotlin/com/myassistant/app/domain/model/InfoCategory.kt

---
+++ app/src/main/kotlin/com/myassistant/app/domain/model/InfoCategory.kt
@@ -0,0 +1,11 @@
+package com.myassistant.app.domain.model
+
+enum class InfoCategory(val label: String) {
+    SCHEDULE("日程"),
+    CONTACT("联系人"),
+    SHOPPING("购物"),
+    ARTICLE("文章"),
+    CODE("代码"),
+    FINANCE("财务"),
+    OTHER("其他")
+}
