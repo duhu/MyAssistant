f68f1e154cd7be9d7d8109e31743ace1378cf3d6
duhu <duhu@live.com>
2026-05-27 10:21:40 +0800

    Add app/src/main/kotlin/com/myassistant/app/MyAssistantApp.kt

---
+++ app/src/main/kotlin/com/myassistant/app/MyAssistantApp.kt
@@ -0,0 +1,28 @@
+package com.myassistant.app
+
+import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class MyAssistantApp : Application(), Configuration.Provider {
+
+    @Inject lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: MyAssistantApp
+            private set
+    }
+}
